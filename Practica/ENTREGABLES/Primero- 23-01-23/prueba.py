ce5f92f8927953becb69522c9548ad0cdd200b0d
Maxi <mxxn13@gmail.com>
2024-01-20 17:26:12 -0300

    avances en pruebas

--- Practica/ENTREGABLES/Primero- 23-01-23/prueba.py
+++ Practica/ENTREGABLES/Primero- 23-01-23/prueba.py
@@ -35,44 +35,38 @@ try:
     response_json = json.loads(paginas.text)
     cantidad_paginas=response_json['paging']['total']+1
     print(cantidad_paginas)
-    #-----------------------------------------------------------------------
-    #for x in range(1,cantidad_paginas):
-     #       respuesta_2 = requests.get(url2, params=params|{'page':x}, headers=headers)
-    respuesta_2 = requests.get(url2, params=params|{'page':1}, headers=headers)
-    #e wady 181822
-            # Verifica si la solicitud fue exitosa (código de estado 200)
-    if respuesta_2.status_code == 200:
-                # Procesa la respuesta de la API
-                lista=[]
+     #-----------------------------------------------------------------------
+    for x in range(1,cantidad_paginas):
+        respuesta_2 = requests.get(url2, params=params|{'page':x}, headers=headers)
+        # Verifica si la solicitud fue exitosa (código de estado 200)
+        if respuesta_2.status_code == 200:
                 response_json = json.loads(respuesta_2.text)
-                lista.append(response_json)
-                respuesta_2 = requests.get(url2, params=params|{'page':2}, headers=headers)
-                response_json2 =  json.loads(respuesta_2.text)
-                lista.append(response_json2)
+                lista_jug.append(response_json)
                 #data_jugadores= response_json['response']
-                print(lista[1])
-                #for i in range(len(data_jugadores)):
-                    #--------------------------------------------------------
-                    #EQUIPOS de la tabla
-                
-                    #FECHA DE ACTUALIZACION DE LA API
-                # fecha_actualizacion=data_jugadores[i]["update"]
-                    #--------------------------------------------------------
-                    #dicc_pos={"id_eq":id_eq,"name_eq":name_eq,"logo_eq":logo_eq,"puesto":puesto,"puntos":puntos,"part_jugados":part_jugados,"part_ganados":part_ganados,"part_empatados":part_empatados,
-                    #          "part_perdidos":part_perdidos,"goles_favor":goles_favor,"goles_contra":goles_contra,"fecha_actualizacion":fecha_actualizacion}
-                    #lista_jug.append(dicc_pos)
-                    #fin del for
+        else:
+            # Muestra un mensaje de error si la solicitud no fue exitosa
+            print("Error en la solicitud.pagina: {x}. Código de estado:", respuesta_2.status_code)
+            
+    #recorrer la lista
+    lista=[]
+    for i in range(0,len(lista_jug)+1):
+        #--------------------------------------------------------  
+         fecha_actualizacion=lista_jug[i]["response"]
+        #--------------------------------------------------------
+        #dicc_pos={"id_eq":id_eq,"name_eq":name_eq,"logo_eq":logo_eq,"puesto":puesto,"puntos":puntos,"part_jugados":part_jugados,"part_ganados":part_ganados,"part_empatados":part_empatados,
+        #          "part_perdidos":part_perdidos,"goles_favor":goles_favor,"goles_contra":goles_contra,"fecha_actualizacion":fecha_actualizacion}
+        #lista_jug.append(dicc_pos)
+    #fin del for
 
-                #df_posiciones = pd.DataFrame(lista_pos)
+    #df_posiciones = pd.DataFrame(lista_pos)
 
-                #Dataframe a redshift
-                #try:
-                #    df_posiciones.to_sql('mxxn13_coderhouse.tabla_posiciones_premier_league',conn,index=False,if_exists='replace')
-                #except:
-                #    print('Error en la carga de datos a redshift')
-    else:
-        # Muestra un mensaje de error si la solicitud no fue exitosa
-        print("Error en la solicitud. Código de estado:", respuesta_2.status_code)
+    #Dataframe a redshift
+    #try:
+    #    df_posiciones.to_sql('mxxn13_coderhouse.tabla_posiciones_premier_league',conn,index=False,if_exists='replace')
+    #except:
+    #    print('Error en la carga de datos a redshift')
+            
+            
 except requests.exceptions.RequestException as e:
     # Muestra un mensaje si hay un error en la solicitud
     print("Error en la solicitud:", e)
