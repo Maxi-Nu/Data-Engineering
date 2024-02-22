b5849ef5a95775f7217310b1d71fa2c5c78061c6
Maximiliano Nuñez <52610820+Maxi-Nu@users.noreply.github.com>
2024-02-22 20:12:08 -0300

    Delete Practica/ENTREGABLES/Segunda-06-02-24/keys.py

--- Practica/ENTREGABLES/Segunda-06-02-24/keys.py
+++
@@ -1,2 +0,0 @@
-api_key= '9c8d551d3c2173d4d742c93a0d86a553'
-redshift_conn='postgresql://mxxn13_coderhouse:S3Be0tbE8y@data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com:5439/data-engineer-database'
